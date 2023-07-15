c496c5546ca73b7ffc4d1a38f956529e3e30af5b
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2023-07-15 10:36:49 +0500

    update

---
+++ program44/index.js
@@ -0,0 +1,6 @@
+function sandWitches(items) {
+    console.log(items);
+}
+console.log(['Tomatoes']);
+console.log(['Tomatoes', 'Lettuce', 'Onions']);
+console.log(['Tomatoes', 'Lettuce', 'Onions', 'Peppers']);
---
+++ program44/index.ts
@@ -0,0 +1,7 @@
+function sandWitches(items: string[]) {
+    console.log(items)
+}
+
+console.log(['Tomatoes'])
+console.log(['Tomatoes','Lettuce','Onions'])
+console.log(['Tomatoes','Lettuce','Onions','Peppers'])
