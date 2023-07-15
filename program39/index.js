c2815f641daef37df6ac035cc1e30294a130bb7c
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2023-07-15 09:10:23 +0500

    update

---
+++ program39/index.js
@@ -0,0 +1,6 @@
+function describe_city(city, country) {
+    return "\"".concat(city, ", ").concat(country, "\"");
+}
+console.log(describe_city('Karachi', 'Pakistan'));
+console.log(describe_city('Beijing', 'China'));
+console.log(describe_city('Kabul', 'Afghanistan'));
---
+++ program39/index.ts
@@ -0,0 +1,7 @@
+function describe_city(city:string , country:string) : string {
+    return  `"${city}, ${country}"`
+}
+
+console.log(describe_city('Karachi','Pakistan'))
+console.log(describe_city('Beijing','China'))
+console.log(describe_city('Kabul','Afghanistan'))
