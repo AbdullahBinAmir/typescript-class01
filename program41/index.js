55feb50f0d6814ef82a6b20543e85425447869ba
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2023-07-15 09:45:06 +0500

    update

---
+++ program41/index.js
@@ -0,0 +1,6 @@
+var show_magicians = function (names) {
+    names.forEach(function (name) {
+        console.log(name);
+    });
+};
+show_magicians(['Ali', 'Ahmed', 'Abdullah', 'Saqib', 'Hamza']);
---
+++ program41/index.ts
@@ -0,0 +1,7 @@
+const show_magicians = (names: string[])=>{
+    names.forEach(name => {
+        console.log(name)
+    });
+}
+
+show_magicians(['Ali','Ahmed','Abdullah','Saqib','Hamza'])
